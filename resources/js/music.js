c5b72b87402daac3c3eab0cf88fcdc3c1f0c1321
frankmarx <frankmarx99@gmail.com>
2021-09-09 20:38:47 -0400

    js and styling of albuminfo for safari

--- resources/js/music.js
+++ resources/js/music.js
@@ -5,10 +5,10 @@ for (i = 0; i < arts.length; i++) {
 	arts[i].addEventListener("click", function () {
 		this.classList.toggle("active");
     if (this.style.opacity === "" || this.style.opacity === "0") {
-      this.style.opacity = "90%";
+      this.style.opacity = ".9";
     } 
 		else {
-      this.style.opacity = "0%";
+      this.style.opacity = "0";
     }
 	});
 }
