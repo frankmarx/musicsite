26b4d95441c69b1fd58654a896e7e460d2eabe82
Gus Marx <gusmarxmusic@gmail.com>
2025-05-25 22:35:42 -0400

    greyouts

--- resources/js/albums.js
+++ resources/js/albums.js
@@ -3,6 +3,7 @@ var songs = document.getElementsByClassName("songs-and-times");
 var i;
 
 for (i = 0; i < songs.length; i++) {
+  if (songs[i].getAttribute("unreleased") != null) continue;
   songs[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
